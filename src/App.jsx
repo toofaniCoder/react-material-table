5a2a20b50d96a23582cec61deff0098640be185a
toofaniCoder <smartcoder500@gmail.com>
2024-04-18 21:28:57 +0530

    add intial code with any table implementation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h3">
+        Vite + React + Material UI + Material React Table V2
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline, GlobalStyles } from "@mui/material";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CssBaseline />
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>
+);
