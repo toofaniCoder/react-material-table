e5a90beca9c492cab0ae394f73a25748b1a2cac0
toofaniCoder <smartcoder500@gmail.com>
2024-05-03 14:28:35 +0530

    row actions feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useMemo } from "react";
-import { Container } from "@mui/material";
+import { Button, Container, MenuItem, Stack } from "@mui/material";
 import "./App.css";
 
 import {
+  MRT_ActionMenuItem,
   MaterialReactTable,
   useMaterialReactTable,
 } from "material-react-table";
 
 import STUDENTS from "./students.json";
+import { Delete, Edit } from "@mui/icons-material";
 // console.log(STUDENTS);
 
 function App() {
@@ -65,9 +67,9 @@ function App() {
         header: "Street Address",
       },
       {
-        accessorKey:"address.state",
-        header:"State Name"
-      }
+        accessorKey: "address.state",
+        header: "State Name",
+      },
     ],
     []
   );
@@ -75,6 +77,47 @@ function App() {
   const table = useMaterialReactTable({
     columns,
     data: STUDENTS,
+    enableRowActions: true,
+    // renderRowActionMenuItems: ({ table }) => [
+    //   // <MenuItem key={"edit"}>edit action</MenuItem>,
+    //   // <MenuItem key={"delete"}>delete action</MenuItem>,
+    //   // <MenuItem key={"disable"}>disable action</MenuItem>,
+
+    //   // <MRT_ActionMenuItem //or just use a normal MUI MenuItem component
+    //   //   icon={<Edit />}
+    //   //   key="edit"
+    //   //   label="Edit"
+    //   //   onClick={() => alert("Edit")}
+    //   //   table={table}
+    //   // />,
+
+    // ],
+    displayColumnDefOptions: {
+      "mrt-row-actions": {
+        header: "Admin Actions", //change header text
+      },
+    },
+    positionActionsColumn: "last",
+    renderRowActions: () => (
+      <Stack direction={"row"} spacing={2}>
+        <Button
+          startIcon={<Edit />}
+          variant="contained"
+          color="success"
+          size="small"
+        >
+          edit
+        </Button>{" "}
+        <Button
+          startIcon={<Delete />}
+          variant="contained"
+          size="small"
+          color="error"
+        >
+          delete
+        </Button>
+      </Stack>
+    ),
     initialState: { pagination: { pageSize: 5, pageIndex: 0 } },
   });
 
