05ed769781b650f80ceeb94790ebc7eca104e2fe
toofaniCoder <smartcoder500@gmail.com>
2024-05-08 12:20:11 +0530

    add all feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import { useMemo } from "react";
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import "./App.css";
 
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-} from "material-react-table";
+import { MaterialReactTable } from "material-react-table";
 
 import STUDENTS from "./students.json";
+import { useLocalStorage } from "@uidotdev/usehooks";
 // console.log(STUDENTS);
 
 function App() {
+  const [sorting, setSorting] = useLocalStorage("mrt_sorting", []);
+  const [columnFilters, setColumnFilters] = useLocalStorage("mrt_sorting", []);
+
+  const [columnVisibility, setColumnVisibility] = useLocalStorage(
+    "mrt_columnVisibility",
+    {}
+  );
+  const [density, setDensity] = useLocalStorage("mrt_density", "comfortable");
+  const [globalFilter, setGlobalFilter] = useLocalStorage(
+    "mrt_globalFilter",
+    ""
+  );
+  const [showGlobalFilter, setShowGlobalFilter] = useLocalStorage(
+    "mrt_showGlobalFilter",
+    false
+  );
+  const [showColumnFilters, setShowColumnFilters] = useLocalStorage(
+    "mrt_showColumnFilters",
+    false
+  );
+
+  const resetState = () => {
+    localStorage.removeItem("mrt_columnFilters");
+    localStorage.removeItem("mrt_columnVisibility");
+    localStorage.removeItem("mrt_density");
+    localStorage.removeItem("mrt_globalFilter");
+    localStorage.removeItem("mrt_showGlobalFilter");
+    localStorage.removeItem("mrt_showColumnFilters");
+    localStorage.removeItem("mrt_sorting");
+    window.location.reload();
+  };
+
   //should be memoized or stable
   const columns = useMemo(
     () => [
@@ -65,22 +95,39 @@ function App() {
         header: "Street Address",
       },
       {
-        accessorKey:"address.state",
-        header:"State Name"
-      }
+        accessorKey: "address.state",
+        header: "State Name",
+      },
     ],
     []
   );
 
-  const table = useMaterialReactTable({
-    columns,
-    data: STUDENTS,
-    initialState: { pagination: { pageSize: 5, pageIndex: 0 } },
-  });
-
   return (
     <Container sx={{ py: 5 }}>
-      <MaterialReactTable table={table} />
+      <MaterialReactTable
+        data={STUDENTS}
+        columns={columns}
+        onColumnFiltersChange={setColumnFilters}
+        onColumnVisibilityChange={setColumnVisibility}
+        onDensityChange={setDensity}
+        onGlobalFilterChange={setGlobalFilter}
+        onShowColumnFiltersChange={setShowColumnFilters}
+        onShowGlobalFilterChange={setShowGlobalFilter}
+        onSortingChange={setSorting}
+        renderTopToolbarCustomActions={() => (
+          <Button onClick={resetState}>Reset State</Button>
+        )}
+        state={{
+          columnFilters,
+          columnVisibility,
+          density,
+          globalFilter,
+          showColumnFilters,
+          showGlobalFilter,
+          sorting,
+        }}
+        initialState={{ pagination: { pageIndex: 0, pageSize: 5 } }}
+      />
     </Container>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,10 @@ import "@fontsource/roboto/700.css";
 import { CssBaseline, GlobalStyles } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <>
+    {" "}
     <CssBaseline />
     <GlobalStyles styles={{ body: { backgroundColor: "#f1f1f1" } }} />
     <App />
-  </React.StrictMode>
+  </>
 );
