3e9a35d42ca1eaba066401b52c227fb8c4eda0fe
toofaniCoder <smartcoder500@gmail.com>
2024-05-16 11:16:32 +0530

    lazy sub rows feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,53 @@
-import { useMemo } from "react";
+/* eslint-disable react/prop-types */
+import { useMemo, useState } from "react";
 import { Container } from "@mui/material";
 import "./App.css";
-
 import {
   MaterialReactTable,
   useMaterialReactTable,
 } from "material-react-table";
+import axios from "axios";
+import { keepPreviousData, useQuery } from "@tanstack/react-query"; //note: this is TanStack React Query V5
 
 import STUDENTS from "./students.json";
 // console.log(STUDENTS);
 
 function App() {
-  //should be memoized or stable
+  const [expanded, setExpanded] = useState({});
+  console.log(expanded);
+
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ["students", ...Object.keys(expanded)],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        Object.keys(expanded)
+          ? `http://localhost:5000/students?parentIds=${Object.keys(
+              expanded
+            ).join(",")}`
+          : `http://localhost:5000/students`
+      );
+      return data;
+    },
+    placeholderData: keepPreviousData,
+  });
+
+  return (
+    <DataTable
+      isFetching={isFetching}
+      data={data}
+      loading={isLoading}
+      expanded={expanded}
+      setExpanded={setExpanded}
+    />
+  );
+}
+
+const DataTable = ({ data, loading, expanded, setExpanded, isFetching }) => {
   const columns = useMemo(
     () => [
       {
         accessorKey: "name",
-        header: "Full Name",
+        header: "Full Name ",
       },
       {
         accessorKey: "email",
@@ -26,17 +57,14 @@ function App() {
         accessorKey: "phone",
         header: "Phone Number",
       },
-
       {
         accessorKey: "standard",
         header: "Class Name",
       },
-
       {
         accessorKey: "section",
-        header: "Section",
+        header: "Section Name",
       },
-
       {
         accessorKey: "age",
         header: "Age",
@@ -45,7 +73,6 @@ function App() {
         accessorKey: "date_of_birth",
         header: "DOB",
       },
-
       {
         accessorKey: "date_of_admission",
         header: "DOA",
@@ -53,16 +80,15 @@ function App() {
 
       {
         accessorKey: "address.pincode",
-        header: "Postal Code",
+        header: "Pin Code",
       },
-
       {
         accessorKey: "address.city",
         header: "City Name",
       },
       {
         accessorKey: "address.street",
-        header: "Street Address",
+        header: "Street Name",
       },
       {
         accessorKey: "address.state",
@@ -73,24 +99,34 @@ function App() {
   );
 
   const rootData = useMemo(
-    () => STUDENTS.filter((student) => !student.parentId),
-    [STUDENTS]
+    () =>
+      data?.students
+        ? data?.students?.filter((student) => student.parentId === null)
+        : [],
+    [data?.students]
   );
-
   const table = useMaterialReactTable({
     columns,
     data: rootData,
     enableExpanding: true,
+    getRowId: (row) => row.studentId,
+    onExpandedChange: setExpanded,
+    getRowCanExpand: (row) => {
+      return row.original.hasChild;
+    },
+    state: {
+      isLoading: loading,
+      showProgressBars: isFetching,
+      expanded: expanded,
+    },
     getSubRows: (row) =>
-      STUDENTS.filter((student) => student.parentId === row.id),
+      data?.students?.filter((student) => student.parentId === row.studentId),
     initialState: { pagination: { pageSize: 5, pageIndex: 0 } },
   });
-
   return (
     <Container sx={{ py: 5 }}>
       <MaterialReactTable table={table} />
     </Container>
   );
-}
-
+};
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { CssBaseline, GlobalStyles } from "@mui/material";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
-    <GlobalStyles styles={{ body: { backgroundColor: "#f1f1f1" } }} />
-    <App />
+    <GlobalStyles styles={{ body: { backgroundColor: "#ddd" } }} />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
