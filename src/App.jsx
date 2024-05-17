98743b807bd3a40e60e94b53fd01c51638e71d02
toofaniCoder <smartcoder500@gmail.com>
2024-05-17 12:51:49 +0530

    export to pdf feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useMemo } from "react";
-import { Container } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
 import "./App.css";
-
+import { flatten } from "flat";
 import {
   MaterialReactTable,
   useMaterialReactTable,
 } from "material-react-table";
-
+import { jsPDF } from "jspdf"; //or use your library of choice here
+import autoTable from "jspdf-autotable";
 import STUDENTS from "./students.json";
 // console.log(STUDENTS);
 
@@ -37,44 +38,78 @@ function App() {
         header: "Section",
       },
 
-      {
-        accessorKey: "age",
-        header: "Age",
-      },
-      {
-        accessorKey: "date_of_birth",
-        header: "DOB",
-      },
+      // {
+      //   accessorKey: "age",
+      //   header: "Age",
+      // },
+      // {
+      //   accessorKey: "date_of_birth",
+      //   header: "DOB",
+      // },
 
-      {
-        accessorKey: "date_of_admission",
-        header: "DOA",
-      },
+      // {
+      //   accessorKey: "date_of_admission",
+      //   header: "DOA",
+      // },
 
       {
         accessorKey: "address.pincode",
         header: "Postal Code",
       },
 
-      {
-        accessorKey: "address.city",
-        header: "City Name",
-      },
-      {
-        accessorKey: "address.street",
-        header: "Street Address",
-      },
-      {
-        accessorKey:"address.state",
-        header:"State Name"
-      }
+      // {
+      //   accessorKey: "address.city",
+      //   header: "City Name",
+      // },
+      // {
+      //   accessorKey: "address.street",
+      //   header: "Street Address",
+      // },
+      // {
+      //   accessorKey: "address.state",
+      //   header: "State Name",
+      // },
     ],
     []
   );
+  const handleExportRows = (rows) => {
+    const doc = new jsPDF({
+      orientation: "landscape",
+    });
+    const tableData = rows.map((row) =>
+      columns.map((column) => row.original[column.accessorKey])
+    );
+    const tableHeaders = columns.map((c) => c.header);
+    autoTable(doc, {
+      head: [tableHeaders],
+      body: tableData,
+    });
 
+    doc.save("mrt-pdf-example.pdf");
+  };
   const table = useMaterialReactTable({
     columns,
     data: STUDENTS,
+    enableRowSelection: true,
+    renderTopToolbarCustomActions: () => (
+      <Box>
+        <Button
+          onClick={() =>
+            handleExportRows(table.getPrePaginationRowModel().rows)
+          }
+        >
+          Export All Rows
+        </Button>{" "}
+        <Button
+          size="small"
+          onClick={() => handleExportRows(table.getRowModel().rows)}
+        >
+          Export Current Page
+        </Button>{" "}
+        <Button      onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
+       size="small">Export Selected Rows</Button>
+      </Box>
+    ),
     initialState: { pagination: { pageSize: 5, pageIndex: 0 } },
   });
 
