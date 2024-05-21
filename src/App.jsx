bbfc0bf8feb6e82f34012cf98b06277d0ffad949
toofaniCoder <smartcoder500@gmail.com>
2024-05-21 09:22:43 +0530

    add complex-dynamic-columns feature

--- src/App.jsx
+++ src/App.jsx
@@ -9,69 +9,80 @@ import {
 
 import STUDENTS from "./students.json";
 // console.log(STUDENTS);
+import { flatten } from "flat";
+import _ from "lodash";
 
 function App() {
   //should be memoized or stable
+  // const columns = useMemo(
+  //   () => [
+  //     {
+  //       accessorKey: "name",
+  //       header: "Full Name",
+  //     },
+  //     {
+  //       accessorKey: "email",
+  //       header: "E-mail Address",
+  //     },
+  //     {
+  //       accessorKey: "phone",
+  //       header: "Phone Number",
+  //     },
+
+  //     {
+  //       accessorKey: "standard",
+  //       header: "Class Name",
+  //     },
+
+  //     {
+  //       accessorKey: "section",
+  //       header: "Section",
+  //     },
+
+  //     {
+  //       accessorKey: "age",
+  //       header: "Age",
+  //     },
+  //     {
+  //       accessorKey: "date_of_birth",
+  //       header: "DOB",
+  //     },
+
+  //     {
+  //       accessorKey: "date_of_admission",
+  //       header: "DOA",
+  //     },
+
+  //     {
+  //       accessorKey: "address.pincode",
+  //       header: "Postal Code",
+  //     },
+
+  //     {
+  //       accessorKey: "address.city",
+  //       header: "City Name",
+  //     },
+  //     {
+  //       accessorKey: "address.street",
+  //       header: "Street Address",
+  //     },
+  //     {
+  //       accessorKey: "address.state",
+  //       header: "State Name",
+  //     },
+  //   ],
+  //   []
+  // );
+
   const columns = useMemo(
-    () => [
-      {
-        accessorKey: "name",
-        header: "Full Name",
-      },
-      {
-        accessorKey: "email",
-        header: "E-mail Address",
-      },
-      {
-        accessorKey: "phone",
-        header: "Phone Number",
-      },
-
-      {
-        accessorKey: "standard",
-        header: "Class Name",
-      },
-
-      {
-        accessorKey: "section",
-        header: "Section",
-      },
-
-      {
-        accessorKey: "age",
-        header: "Age",
-      },
-      {
-        accessorKey: "date_of_birth",
-        header: "DOB",
-      },
-
-      {
-        accessorKey: "date_of_admission",
-        header: "DOA",
-      },
-
-      {
-        accessorKey: "address.pincode",
-        header: "Postal Code",
-      },
-
-      {
-        accessorKey: "address.city",
-        header: "City Name",
-      },
-      {
-        accessorKey: "address.street",
-        header: "Street Address",
-      },
-      {
-        accessorKey:"address.state",
-        header:"State Name"
-      }
-    ],
+    () =>
+      Object.keys(flatten(STUDENTS.at(0))).map((columnId) => ({
+        accessorKey: columnId,
+        header: _.capitalize(_.lowerCase(columnId.split(".").pop())),
+        id: columnId,
+      })),
     []
   );
-
   const table = useMaterialReactTable({
     columns,
     data: STUDENTS,
